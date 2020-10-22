db4a064e32bec4a3be2a6fa4194f725730d1a8bb
NkvXness <leonakavexmusic@gmail.com>
2020-10-22 22:39:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let ch = bracketsConfig[i].join('');
+ 
+    if (str.includes(ch)){
+      str = str.replace(ch, '');
+      i = -1;
+    }
+  }
+  return str ? false : true;
 }
